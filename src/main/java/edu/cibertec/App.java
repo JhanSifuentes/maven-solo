6d8cf2984dd09e78f35f0ff97ba49ba92208a627
JhanSifuentes <sgsifuentesjc00@gmail.com>
2024-06-02 22:23:27 -0500

    initial scaffold

---
+++ src/main/java/edu/cibertec/App.java
@@ -0,0 +1,11 @@
+package edu.cibertec;
+
+import org.apache.commons.collections4.Unmodifiable;
+
+public class App implements Unmodifiable
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
